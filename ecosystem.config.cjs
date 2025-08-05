7954024810144afda0ab05addd74c4901e129a52
Nick Geerts <info>
2025-08-05 23:34:15 +0200

    feat: PM2 config

---
+++ ecosystem.config.cjs
@@ -0,0 +1,36 @@
+const name = 'nickbreed'
+const script = 'node_modules/.bin/next'
+const port = 3010
+const args = `start -p ${port}`
+const repo = 'git@github.com:nickgeerts/soundtrout.git'
+const build = 'npm install && npx prisma migrate deploy && npx prisma generate && npm run build'
+
+module.exports = {
+  apps: [
+    {
+      name,
+      script,
+      args,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: port
+      }
+    }
+  ],
+
+  deploy: {
+    production: {
+      user: 'deploy',
+      host: 'nickgeerts.com',
+      ref: 'origin/master',
+      repo,
+      ssh_options: 'ForwardAgent=yes',
+      path: `/home/deploy/${name}`,
+      'post-deploy': `${build} && pm2 reload ecosystem.config.cjs --env production && pm2 save`
+    }
+  }
+}
